b554f3f562f9bb073b61907cb3c68a0c57e0675e
dpradell_dev <dpradell.dev@gmail.com>
2024-01-23 13:53:47 +0100

    added Recovery Azure Functions

---
+++ serverless-backend/CompleteRecovery/index.ts
@@ -0,0 +1,52 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions";
+import Openfort, { CompleteRecoveryRequest } from "@openfort/openfort-node";
+
+const openfort = new Openfort(process.env.OPENFORT_API_KEY);
+
+const httpTrigger: AzureFunction = async function (
+  context: Context,
+  req: HttpRequest
+): Promise<void> {
+  try {
+    if (
+      !req.body ||
+      !req.body.CallerEntityProfile.Lineage.MasterPlayerAccountId ||
+      !req.body.FunctionArgument.accountId ||
+      !req.body.FunctionArgument.newOwnerAddress
+    ) {
+      context.res = {
+        status: 400,
+        body: "Please pass a valid request body",
+      };
+      return;
+    }
+
+    context.log("HTTP trigger function processed a request.");
+
+    const accountId = req.body.FunctionArgument.accountId;
+    const newOwnerAddress = req.body.FunctionArgument.newOwnerAddress;
+    
+    const recoveryRequest: CompleteRecoveryRequest = {
+      accountId: accountId,
+      newOwnerAddress: newOwnerAddress,
+      policy: process.env.OF_TX_SPONSOR
+      //TODO signatures: ?
+    };
+
+    const transactionIntent = await openfort.accounts.completeRecovery(recoveryRequest);
+
+    context.log("API call was successful.");
+    context.res = {
+      status: 200,
+      body: transactionIntent.response.transactionHash,
+    };
+  } catch (error) {
+    context.log(error);
+    context.res = {
+      status: 500,
+      body: JSON.stringify(error),
+    };
+  }
+};
+
+export default httpTrigger;
