b881f4488c1fd0e083530fd9e2bf33281a8ae079
dpradell <dpradell.dev@gmail.com>
2024-03-27 19:34:52 +0100

    openfort controller

--- unity-game/Assets/_Game/Scripts/Openfort/OpenfortController.cs
+++ unity-game/Assets/_Game/Scripts/Openfort/OpenfortController.cs
@@ -1,7 +1,44 @@
-namespace _Game.Scripts.Openfort
+using Cysharp.Threading.Tasks;
+using Openfort;
+using Openfort.Model;
+using Openfort.Recovery;
+using UnityEngine;
+
+public class OpenfortController: MonoBehaviour
 {
-    public class OpenfortController
+    public static OpenfortController Instance { get; private set; }
+    
+    private OpenfortSDK _openfort;
+    private string _oauthAccessToken;
+    
+    private void Awake()
     {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    
+    public async UniTask AuthenticateWithOAuth(string idToken)
+    {
+        Debug.Log("PlayFab session ticket: " + idToken);
+    
+        _openfort = new OpenfortSDK(OFStaticData.PublishableKey); 
+        _oauthAccessToken = await _openfort.AuthenticateWithOAuth(OAuthProvider.Playfab, idToken, TokenType.IdToken);
+        Debug.Log("Access Token: " + _oauthAccessToken);
         
+        try
+        {
+            _openfort.ConfigureEmbeddedSigner(4337);
+        }
+        catch (MissingRecoveryMethod)
+        {
+            await _openfort.ConfigureEmbeddedRecovery(new PasswordRecovery("secret"));
+        }
     }
 }
